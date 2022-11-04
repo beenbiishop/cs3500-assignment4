5047f6b2086cde23a026ee86f531e960c2b77bf3
sm1se <rosemary.s@northeastern.edu>
2022-11-04 12:28:31 -0400

    finished the view tests!

--- test/view/ImageProcessorViewImplTest.java
+++ test/view/ImageProcessorViewImplTest.java
@@ -10,15 +10,32 @@ import org.junit.Test;
  */
 public class ImageProcessorViewImplTest {
 
+  ImageProcessorView view;
+  Appendable appendable;
+
   @Before
   public void setUp() {
+    this.appendable = new StringBuilder();
+    this.view = new ImageProcessorViewImpl(appendable);
   }
 
   @Test
   public void renderMessage() {
+    this.view.renderMessage("Message to Render.");
+    assertEquals("Message to Render.", this.appendable.toString());
   }
 
   @Test
   public void renderMenu() {
+    this.view.renderMenu();
+    assertEquals("quit - quits the program" + System.lineSeparator() +
+            "load <path> <filename> - loads an image (identified by given name) into the processor" + System.lineSeparator() +
+            "save <path> <filename> - saves an image to an output file" + System.lineSeparator() +
+            "menu - displays the menu of commands" + System.lineSeparator() +
+            "visualize-<component> <filename> <new filename> - transforms an image to a new image greyscaled using a chosen component" + System.lineSeparator() +
+            "brighten <amount> <filename> <new filename> - transforms an image to a new image brightened by an amount" + System.lineSeparator() +
+            "darken <amount> <filename> <new filename> - transforms an image to a new image darkened by an amount" + System.lineSeparator() +
+            "horizontal-flip <filename> <new filename> - horizontally flips an image to a new image" + System.lineSeparator() +
+            "vertical-flip <filename> <new filename> - vertically flips an image to a new image" + System.lineSeparator(), this.appendable.toString());
   }
 }
